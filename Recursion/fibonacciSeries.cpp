eadb634f871c0289725734b466540ed3f3dcdf2e
Muhammad Shahzaib <imshahzaib101@gmail.com>
2022-12-11 21:03:56 +0500

    fibonacii nth term

---
+++ Recursion/fibonacciSeries.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int fabonaciiNthTerm(int n)
+{
+    if (n < 1)
+    {
+        return 0;
+    }
+    if (n == 1)
+    {
+        return 1;
+    }
+    int ans = fabonaciiNthTerm(n - 1) + fabonaciiNthTerm(n - 2);
+    return ans;
+}
+int main()
+{
+    int n;
+    cout << "Enter nth term, you want to find of Fibonacii series" << endl;
+    cin >> n;
+    cout << "The " << n << "th term of fibonacii series is: " << fabonaciiNthTerm(n);
+    return 0;
+}
